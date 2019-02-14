f0f94fce0deb9795407d3058eabf7ef32f86a43f
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-02-14 08:20:07 +0200

    Helper files to solve the project

---
+++ SolutionToProject/MyButton.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CanvasProject;
+
+namespace CanvasProject
+{
+    /// <summary>
+    /// This class represents a Buttons consist of two points
+    /// Top-left and Bottom-right
+    /// The width and height are calculated automatically upon instance creation and points modification
+    /// The Setters validates the Top-left and Bottom-right points
+    /// </summary>
+    internal class MyButton
+    {
+
+        /// protected data topleft and bottomright
+        protected Point _topLeft;
+        protected Point _bottomRight;
+
+        /// private data - width + height
+        private int _width;
+        private int _height;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="topLeft">Top-left coordinates of the new button</param>
+        /// <param name="bottomRight">bottom-right coordinates of the new button</param>
+        internal MyButton(Point topLeft, Point bottomRight)
+        {
+            _topLeft = topLeft;
+            _bottomRight = bottomRight;
+            UpdateWidthAndHeight();
+        }
+
+        /// <summary>
+        /// Returns the auto calculated width of the button (between top-left and bottom-right)
+        /// </summary>
+        /// <returns>button width</returns>
+        internal int GetWidth()
+        {
+            return _width;
+        }
+
+        /// <summary>
+        /// Returns the auto calculated height of the button (between top-left and bottom-right)
+        /// </summary>
+        /// <returns>button height</returns>
+        internal int GetHeight()
+        {
+            return _height;
+        }
+
+        /// <summary>
+        /// top-left point getter
+        /// </summary>
+        /// <returns>topleft point</returns>
+        internal Point GetTopLeft()
+        {
+            return _topLeft;
+        }
+
+        /// <summary>
+        /// bottom-right point getter
+        /// </summary>
+        /// <returns>bottomright point</returns>
+        internal Point GetbottomRight()
+        {
+            return _bottomRight;
+        }
+
+        /// <summary>
+        /// Updates the width and height of the button
+        /// </summary>
+        private void UpdateWidthAndHeight()
+        {
+            this._width = _bottomRight.GetX() - _topLeft.GetX();
+            this._height = _bottomRight.GetY() - _topLeft.GetY();
+        }
+
+        /// <summary>
+        /// bottom-right setter
+        /// </summary>
+        /// <param name="br">the new bottomright point</param>
+        /// <returns>true if valid</returns>
+        internal bool SetBottomRight(Point br)
+        {
+            if (br.GetX() < this._topLeft.GetX() || br.GetY() < this._topLeft.GetY())
+            {
+                return false;
+            }
+
+            this._bottomRight = br;
+
+            this.UpdateWidthAndHeight();
+
+            return true;
+        }
+
+        /// <summary>
+        /// top-left setter
+        /// </summary>
+        /// <param name="tl">the new topleft point</param>
+        /// <returns>true if valid</returns>
+        internal bool SetTopLeft(Point tl)
+        {
+            if (tl.GetX() > this._bottomRight.GetX() || tl.GetY() > this._bottomRight.GetY())
+            {
+                return false;
+            }
+
+            this._topLeft = tl;
+
+            this.UpdateWidthAndHeight();
+
+            return true;
+        }
+
+        /// <summary>
+        /// String representation of the class
+        /// </summary>
+        /// <returns>MyButton text representation</returns>
+        public override string ToString()
+        {
+            return $"Button TopLeft : { this._topLeft }, bottomRight : { this._bottomRight }, Width : { this._width }, Height : { this._height }";
+        }
+    }
+}
---
+++ SolutionToProject/Point.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasProject
+{
+    /// <summary>
+    /// This class represents a Point 
+    /// Point contains of x, y coordinates within the Canvas range
+    /// </summary>
+    internal class Point
+    {
+        /// private data - x, y coordinates
+        private int _x;
+        private int _y;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="x">x coordinate of the new point</param>
+        /// <param name="y">y coordinate of the new point</param>
+        internal Point(int x, int y)
+        {
+           _x = x;
+           _y = y;
+        }
+
+        /// <summary>
+        /// X Getter
+        /// </summary>
+        /// <returns>returns the X value of the point</returns>
+        internal int GetX()
+        {
+            return _x;
+        }
+
+        /// <summary>
+        /// Y Getter
+        /// </summary>
+        /// <returns>returns the y value of the point</returns>
+        internal int GetY()
+        {
+            return _y;
+        }
+
+
+        /// <summary>
+        /// X Setter
+        /// </summary>
+        /// <param name="x">the new x coordiante of the point (must be between 0-canvas max width)</param>
+        internal void SetX(int x)
+        {
+            if (x >= 0 && x < MyCanvas.MAX_WIDTH)
+            {
+                _x = x;
+            }
+            else
+            {
+                Console.WriteLine("$x value of {x} is invalid!");
+            }
+        }
+
+
+        /// <summary>
+        /// Y Setter
+        /// </summary>
+        /// <param name="y">the new y coordiante of the point (must be between 0-canvas max height)</param>
+        internal void SetY(int y)
+        {
+            if (y >= 0 && y < MyCanvas.MAX_HEIGHT)
+            {
+                _y = y;
+            }
+            else
+            {
+                Console.WriteLine("$y value of {y} is invalid!");
+            }
+        }
+
+        /// <summary>
+        /// String representation of the class
+        /// </summary>
+        /// <returns>Point text representation</returns>
+        public override string ToString()
+        {
+            return $" Point ( { this._x } , { this._y } )";
+        }
+    }
+}
